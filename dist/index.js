cb6fe1b56cf831c38732495a07b8999cce909737
Subash Poudel <subash.poudel@seattleapplab.com>
2019-02-18 13:48:52 +0545

    Add dist folder

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _MonacoEditor = require('./MonacoEditor');
+
+var _MonacoEditor2 = _interopRequireDefault(_MonacoEditor);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _MonacoEditor2.default;
